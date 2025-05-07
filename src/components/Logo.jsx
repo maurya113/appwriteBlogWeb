b85776ffd8414431a308f5268f1d2dae7f9d882e
manish <manishmauryaa113@gmail.com>
2025-05-07 13:53:40 +0530

    remove span from header

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -11,7 +11,7 @@ export default function Logo({width = '100px', imgSrc="", alt="logo"}) {
             alt={alt} />
         
         }
-            <span className="text-xl font-semibold px-2 py-1 text-gray-800 bg-blue-100 hover:bg-blue-200 transition duration-300 ease-in-out rounded-lg"></span>
+            
 
         </div>
     )
